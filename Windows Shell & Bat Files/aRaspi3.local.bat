cb8f831dcb693b445ec739680d8b76a20d4030ba
ImogenWren <dec.h38@gmail.com>
2022-05-05 19:09:19 +0100

    added loop with user query

--- Windows Shell & Bat Files/aRaspi3.local.bat
+++ Windows Shell & Bat Files/aRaspi3.local.bat
@@ -1,6 +1,12 @@
 @echo off
 title raspi3_ssh
+:start
 echo Attempting Connection with raspberrypi3.local
 echo for help see: https://www.makeuseof.com/tag/essential-windows-cmd-commands/
 ssh pi@raspberrypi3.local
+echo Closing Connection...
+set choice=
+set /p choice="Do you want to restart? Press 'y' and enter for Yes: "
+if not '%choice%'=='' set choice=%choice:~0,1%
+if '%choice%'=='y' goto start
 pause
